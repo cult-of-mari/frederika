832c833408b15924910eaf829fe311d521f5ccca
Denis Strizhkin <strdenis02@gmail.com>
2024-12-28 09:33:59 +0000

    respond to mention

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,27 @@ mod config;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    env_logger::init();
+
     let config_path = PathBuf::from("./Config.toml");
     let config = config::load_config(&config_path)?;
 
-    log::info!("Starting throw dice bot...");
+    log::info!("Starting the bot...");
     let bot = Bot::new(config.telegram.token);
 
     teloxide::repl(bot, |bot: Bot, msg: Message| async move {
-        let me = bot.get_me().await?;
-        let is_mention = msg
-            .mentioned_users()
-            .filter(|user| user.id == me.id)
-            .count()
-            == 1;
-        if is_mention {
-            bot.send_dice(msg.chat.id).await?;
-        }
+        if let Some(text) = msg.text() {
+            log::debug!("Recieved message: {text}");
+            let me = bot.get_me().await?;
+            log::debug!("{me:?}");
+            let is_mention = text.contains(me.username());
+            log::debug!("Is mention: {is_mention}");
+            if is_mention {
+                bot.send_dice(msg.chat.id).await?;
+                bot.send_message(msg.chat.id, "Nipah ^_^").await?;
+            }
+        };
+
         Ok(())
     })
     .await;
