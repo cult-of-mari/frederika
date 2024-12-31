74ee706a01c4ecf516874a2a64d5c6c20987e284
Denis Strizhkin <strdenis02@gmail.com>
2024-12-31 02:14:29 +0000

    new build history

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 use aho_corasick::AhoCorasick;
-use anyhow::Result;
+use anyhow::{anyhow, Result};
+use futures_util::StreamExt;
 use google_gemini::{
     GeminiClient, GeminiMessage, GeminiPart, GeminiRequest, GeminiRole, GeminiSafetySetting,
     GeminiSafetyThreshold, GeminiSystemPart,
 };
 use serde::Serialize;
-use std::sync::{Arc, Mutex};
+use std::{
+    borrow::Borrow,
+    sync::{Arc, Mutex},
+};
 use teloxide::{
     dispatching::UpdateFilterExt,
     prelude::*,
-    types::{ChatKind, Me, MessageId, ParseMode},
+    types::{ChatKind, Me, MediaKind, MessageId, MessageKind, ParseMode},
 };
 
 mod cli;
@@ -55,16 +59,17 @@ impl BotState {
     }
 
     fn should_reply(&self, msg: &Message) -> bool {
-        matches!(msg.chat.kind, ChatKind::Private(_))
-            || msg
-                .reply_to_message()
-                .and_then(|msg| msg.from.clone())
-                .map(|user| user.eq(&self.me))
-                .unwrap_or(false)
-            || self.name_matcher.is_match(msg.text().unwrap())
+        matches!(msg.kind, MessageKind::Common(_))
+            && (matches!(msg.chat.kind, ChatKind::Private(_))
+                || msg
+                    .reply_to_message()
+                    .and_then(|msg| msg.from.as_ref())
+                    .map(|user| user.eq(&self.me))
+                    .unwrap_or(false)
+                || self.name_matcher.is_match(msg.text().unwrap_or_default()))
     }
 
-    async fn get_gemini_reply(&self, msg: &Message) -> String {
+    async fn get_gemini_reply(&self, bot: &Bot, msg: &Message) -> String {
         let mut request = GeminiRequest::default();
 
         request.system_instruction.parts.push(GeminiSystemPart {
@@ -83,12 +88,10 @@ impl BotState {
 
         request.safety_settings.extend(settings);
 
-        let message_history = self.build_message_history(msg);
-        message_history.iter().cloned().for_each(|(role, content)| {
-            let parts = vec![GeminiPart::from(content)];
-            request.contents.push(GeminiMessage::new(role, parts));
-        });
-
+        self.build_message_history(bot, msg)
+            .await
+            .into_iter()
+            .for_each(|msg| request.contents.push(msg));
         let reply_text = match self.gemini.generate(request).await {
             Ok(content) => {
                 log::debug!("Response content: {content}");
@@ -99,33 +102,70 @@ impl BotState {
         BotState::sanitize_text(reply_text.as_str())
     }
 
-    fn build_message_history(&self, last_msg: &Message) -> Vec<(GeminiRole, String)> {
-        self.msg_cache
-            .lock()
-            .unwrap()
+    async fn build_message_history(&self, bot: &Bot, last_msg: &Message) -> Vec<GeminiMessage> {
+        let mut msg_cache = self.msg_cache.lock().unwrap();
+        let futures = msg_cache
             .messages(last_msg.chat.id)
             .chain([last_msg])
-            .filter_map(|msg| {
-                msg.from
-                    .clone()
-                    .map(|user| MessageInfo {
-                        user_name: user.full_name(),
-                        user_id: user.id,
-                        message_content: msg.text().unwrap_or_default(),
-                        message_id: msg.id,
-                    })
-                    .map(|info| {
-                        (
-                            if info.user_id.eq(&self.me.id) {
-                                GeminiRole::Model
-                            } else {
-                                GeminiRole::User
-                            },
-                            serde_json::to_string(&info).unwrap(),
-                        )
-                    })
-            })
+            .map(|msg| self.message_into_gemini_message(bot, msg));
+        futures_util::stream::iter(futures)
+            .buffered(3)
+            .collect::<Vec<_>>()
+            .await
+            .into_iter()
+            .filter_map(|msg| msg.inspect_err(|e| log::debug!("{e}")).ok())
             .collect()
+    }
+
+    async fn message_into_gemini_message(
+        &self,
+        _bot: &Bot,
+        msg: &Message,
+    ) -> Result<GeminiMessage> {
+        let message_id = msg.id;
+        let (user_name, user_id) = msg
+            .from
+            .as_ref()
+            .map(|u| (u.full_name(), u.id))
+            .ok_or_else(|| anyhow!("Message has no author"))?;
+        let MessageKind::Common(msg) = msg.kind.borrow() else {
+            return Err(anyhow!("Unsupported message type (Not Common)"));
+        };
+        let parts = match msg.media_kind.borrow() {
+            MediaKind::Text(text) => {
+                let info = MessageInfo {
+                    user_name,
+                    user_id,
+                    message_content: &text.text,
+                    message_id,
+                };
+                vec![GeminiPart::from(serde_json::to_string(&info)?)]
+            }
+            _ => return Err(anyhow!("Unsupported media type (Not Text)")),
+        };
+        let role = if user_id.eq(&self.me.id) {
+            GeminiRole::Model
+        } else {
+            GeminiRole::User
+        };
+        //         MediaKind::Photo(photo) => {
+        //             let file_id = photo
+        //                 .photo
+        //                 .iter()
+        //                 .max_by_key(|p| p.file.size)
+        //                 .unwrap()
+        //                 .file
+        //                 .id
+        //                 .clone();
+        //             let file_url = bot
+        //                 .get_file(file_id.as_str())
+        //                 .await
+        //                 .inspect_err(|e| log::debug!("{e}"))
+        //                 .map(|url| (url.path));
+        //             todo!()
+        //         }
+
+        Ok(GeminiMessage::new(role, parts))
     }
 
     fn sanitize_text(s: &str) -> String {
@@ -140,23 +180,21 @@ async fn handle_message(
     msg: Message,
     state: Arc<BotState>,
 ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    if let Some(text) = msg.text() {
-        log::debug!("Recieved message: {text}");
-        log::debug!("ChatId: {}", msg.chat.id);
-        if state.should_reply(&msg) {
-            let reply = state.get_gemini_reply(&msg).await;
-            log::debug!("Reply: {reply}");
-            if let Err(error) = bot
-                .send_message(msg.chat.id, reply)
-                .parse_mode(ParseMode::Html)
-                .await
-            {
-                log::error!("Failed to send message: {error}");
-            }
+    log::debug!("{msg:?}");
+    log::debug!("ChatId: {}", msg.chat.id);
+    if state.should_reply(&msg) {
+        let reply = state.get_gemini_reply(&bot, &msg).await;
+        log::debug!("Reply: {reply}");
+        if let Err(error) = bot
+            .send_message(msg.chat.id, reply)
+            .parse_mode(ParseMode::Html)
+            .await
+        {
+            log::error!("Failed to send message: {error}");
         }
-        let mut msg_cache = state.msg_cache.lock().unwrap();
-        msg_cache.add(msg);
-    };
+    }
+    let mut msg_cache = state.msg_cache.lock().unwrap();
+    msg_cache.add(msg);
     Ok(())
 }
 
